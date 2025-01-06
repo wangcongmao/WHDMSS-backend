33768c02053262463177a46dd46547f1a03b97b6
wangcongmao <2772814676@qq.com>
2025-01-06 17:41:37 +0800

    设备数据结构完成

--- web/src/main/java/com/jeesite/modules/dataservice/web/DataserviceDeviceStructureController.java
+++ web/src/main/java/com/jeesite/modules/dataservice/web/DataserviceDeviceStructureController.java
@@ -1,5 +1,6 @@
 package com.jeesite.modules.dataservice.web;
 
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,7 +32,7 @@ public class DataserviceDeviceStructureController extends BaseController {
 
 	@Autowired
 	private DataserviceDeviceStructureService dataserviceDeviceStructureService;
-	
+
 	/**
 	 * 获取数据
 	 */
@@ -39,7 +40,7 @@ public class DataserviceDeviceStructureController extends BaseController {
 	public DataserviceDeviceStructure get(String id, boolean isNewRecord) {
 		return dataserviceDeviceStructureService.get(id, isNewRecord);
 	}
-	
+
 	/**
 	 * 查询列表
 	 */
@@ -49,7 +50,7 @@ public class DataserviceDeviceStructureController extends BaseController {
 		model.addAttribute("dataserviceDeviceStructure", dataserviceDeviceStructure);
 		return "modules/dataservice/dataserviceDeviceStructureList";
 	}
-	
+
 	/**
 	 * 查询列表数据
 	 */
@@ -59,6 +60,63 @@ public class DataserviceDeviceStructureController extends BaseController {
 	public Page<DataserviceDeviceStructure> listData(DataserviceDeviceStructure dataserviceDeviceStructure, HttpServletRequest request, HttpServletResponse response) {
 		dataserviceDeviceStructure.setPage(new Page<>(request, response));
 		Page<DataserviceDeviceStructure> page = dataserviceDeviceStructureService.findPage(dataserviceDeviceStructure);
+		List<DataserviceDeviceStructure> list = page.getList();
+		for (int i = 0; i < list.size(); i++) {
+			DataserviceDeviceStructure dataserviceDeviceStructure1 = list.get(i);
+			String structureData = dataserviceDeviceStructure1.getStructureData();
+			String[] strings = structureData.split("prm");
+			for (int i1 = 1; i1 < strings.length; i1++) {
+				// ":"a","type":"number"},{"
+				String[] strings1 = strings[i1].split("type");
+				// ":"a","
+				String substring1 = strings1[0].substring(3, strings1[0].lastIndexOf(",")-1);
+				// ":"number"},{"
+				String substring2 = strings1[1].substring(3, strings1[1].lastIndexOf("}") - 1);
+				strings[i1] = substring1 + ":" + substring2;
+				if (i1 != strings.length-1) {
+					strings[i1] += " | ";
+				}
+			}
+			String a = "";
+			for (int i1 = 1; i1 < strings.length; i1++) {
+				a += strings[i1];
+			}
+			dataserviceDeviceStructure1.setStructureData(a.substring(0, a.lastIndexOf("\"")));
+		}
+		return page;
+	}
+
+	/**
+	 * 查询列表数据
+	 */
+	@RequiresPermissions("dataservice:deviceStructure:view")
+	@RequestMapping(value = "listDataForTable")
+	@ResponseBody
+	public Page<DataserviceDeviceStructure> listDataForTable(DataserviceDeviceStructure dataserviceDeviceStructure, HttpServletRequest request, HttpServletResponse response) {
+		dataserviceDeviceStructure.setPage(new Page<>(request, response));
+		Page<DataserviceDeviceStructure> page = dataserviceDeviceStructureService.findPage(dataserviceDeviceStructure);
+		List<DataserviceDeviceStructure> list = page.getList();
+		for (int i = 0; i < list.size(); i++) {
+			DataserviceDeviceStructure dataserviceDeviceStructure1 = list.get(i);
+			String structureData = dataserviceDeviceStructure1.getStructureData();
+			String[] strings = structureData.split("prm");
+			for (int i1 = 1; i1 < strings.length; i1++) {
+				// ":"a","type":"number"},{"
+				String[] strings1 = strings[i1].split("type");
+				// ":"a","
+				String substring1 = strings1[0].substring(3, strings1[0].lastIndexOf(",")-1);
+				// ":"number"},{"
+				String substring2 = strings1[1].substring(3, strings1[1].lastIndexOf("}") - 1);
+				strings[i1] = substring1 + ":" + substring2;
+				if (i1 != strings.length-1) {
+					strings[i1] += " | ";
+				}
+			}
+			String a = new String(Arrays.toString(strings));
+			dataserviceDeviceStructure1.setStructureData(a);
+		}
+
+		System.out.println(0);
 		return page;
 	}
 
@@ -82,7 +140,7 @@ public class DataserviceDeviceStructureController extends BaseController {
 		dataserviceDeviceStructureService.save(dataserviceDeviceStructure);
 		return renderResult(Global.TRUE, text("保存设备数据结构成功！"));
 	}
-	
+
 	/**
 	 * 删除数据
 	 */
@@ -93,5 +151,5 @@ public class DataserviceDeviceStructureController extends BaseController {
 		dataserviceDeviceStructureService.delete(dataserviceDeviceStructure);
 		return renderResult(Global.TRUE, text("删除设备数据结构成功！"));
 	}
-	
+
 }
